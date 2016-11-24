ea9714f853f450680dbb1ca04bfcc668704de531
fancymax <lindahai_max@126.com>
2016-11-24 16:14:29 +0800

    update

---
+++ 002-add-two-numbers/add-two-numbers.swift
@@ -0,0 +1,73 @@
+# -*- coding:utf-8 -*-
+
+
+# You are given two linked lists representing two non-negative numbers. The digits are stored in reverse order and each of their nodes contain a single digit. Add the two numbers and return it as a linked list.
+#
+# Input: (2 -> 4 -> 3) + (5 -> 6 -> 4)
+# Output: 7 -> 0 -> 8
+
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     public var val: Int
+ *     public var next: ListNode?
+ *     public init(_ val: Int) {
+ *         self.val = val
+ *         self.next = nil
+ *     }
+ * }
+ */
+class Solution {
+    func addTwoNumbers(l1: ListNode?, _ l2: ListNode?) -> ListNode? {
+        if(l1==nil)&&(l2==nil) {
+            return nil
+        }
+        
+        var ll1 = l1
+        var ll2 = l2
+        
+        let head:ListNode? = ListNode(0)
+        var point = head
+        var previousEndPoint: ListNode?
+        
+        var carry = 0
+        while (ll1 != nil) && (ll2 != nil) {
+            point!.val = (ll1!.val + ll2!.val + carry)%10
+            carry = (ll1!.val + ll2!.val + carry)/10
+            point!.next = ListNode(0)
+            previousEndPoint = point
+            point = point!.next
+            ll1 = ll1!.next
+            ll2 = ll2!.next
+        }
+        
+        while ll2 != nil {
+            point!.val = (ll2!.val + carry)%10
+            carry = (ll2!.val + carry)/10
+            point!.next = ListNode(0)
+            previousEndPoint = point
+            point = point!.next
+            ll2 = ll2!.next
+        }
+        
+        while ll1 != nil {
+            point!.val = (ll1!.val + carry)%10
+            carry = (ll1!.val + carry)/10
+            point!.next = ListNode(0)
+            previousEndPoint = point
+            point = point!.next
+            ll1 = ll1!.next
+        }
+        
+        if carry > 0 {
+            point!.val = carry
+        }
+        else{
+            previousEndPoint?.next = nil
+        }
+        
+        return head
+        
+    }
+}
---
+++ 003-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.swift
@@ -0,0 +1,35 @@
+# -*- coding:utf-8 -*-
+
+
+# Given a string, find the length of the longest substring without repeating characters.
+#
+# Examples:
+#
+# Given "abcabcbb", the answer is "abc", which the length is 3.
+#
+# Given "bbbbb", the answer is "b", with the length of 1.
+#
+# Given "pwwkew", the answer is "wke", with the length of 3. Note that the answer must be a substring, "pwke" is a subsequence and not a substring.
+
+
+class Solution {
+    func lengthOfLongestSubstring(_ s: String) -> Int {
+        
+        var characterDict = [Character:Int]()
+        var maxLength = 0
+        var lastRepeatPos = -1
+        var i = 0
+        
+        for c in s.characters {
+            if (characterDict[c] != nil) && (characterDict[c]! > lastRepeatPos) {
+                lastRepeatPos = characterDict[c]!
+            }
+            
+            maxLength = max(i - lastRepeatPos, maxLength)
+            characterDict[c] = i
+            i += 1
+        }
+        
+        return maxLength
+    }
+}
